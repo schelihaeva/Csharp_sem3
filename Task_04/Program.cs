8ffb17f6fcfe32c45b87ef3c6a2331543ad497da
Anastasya <gbshell2022@gmail.com>
2023-10-16 14:05:27 +0300

    решение

---
+++ Task_04/Program.cs
@@ -0,0 +1,26 @@
+﻿// // Внутри класса Answer напишите метод ShowCube, который принимает на вход число (N) и выводит таблицу кубов чисел от 1 до N (включительно) каждое на новой строке.
+
+
+// N = 3 
+// /*
+// 1
+// 8
+// 27
+// */
+
+// N = 5
+// /*
+// 1
+// 8
+// 27
+// 64
+// 125
+// */
+
+System.Console.WriteLine("Введите число: ");
+ int number = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+
+ for(int i = 1; i <= number; i++)
+ {
+    System.Console.WriteLine($"{Math.Pow(i, 3)} ");
+ }
