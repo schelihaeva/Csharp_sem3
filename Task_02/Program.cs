72c70e792eb4b8b33cbc96c7cdbb5cfbf27e75de
Anastasya <gbshell2022@gmail.com>
2023-10-16 10:26:59 +0300

    решение задачи про число и возведение в квадрат

---
+++ Task_02/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, ктороая принимает на вход число (N) и выдает таблицу квадратов числе от 1 до N
+// 5-> 1,4,9,16,25
+// 2->1,4
+
+System.Console.WriteLine("Введите число: ");
+int number = Math.Abs(Convert.ToInt32(Console.ReadLine()));// Math.Abs -сконвертировали отрицательное уисло в положительное
+int i = 1;
+
+while(i <= number) 
+{
+System.Console.Write($"{Math.Pow(i, 2)} ");
+i++;
+}
+
