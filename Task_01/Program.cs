bd8f63668640ffefd163aa56335d4aaa1fcb677b
Anastasya <gbshell2022@gmail.com>
2023-10-16 10:04:22 +0300

    приведение типов

--- Task_01/Program.cs
+++ Task_01/Program.cs
@@ -4,17 +4,23 @@
 
 
 
-int[] A = new int[2];
-System.Console.Write("Введите координтату x точки А: ");
-A[0] = Convert.ToInt32(Console.ReadLine());
-System.Console.Write("Введите координтату y точки A: ");
-A[1] = Convert.ToInt32(Console.ReadLine());
-int[] B = new int[2];
-System.Console.Write("Введите координтату x точки B: ");
-B[0] = Convert.ToInt32(Console.ReadLine());
-System.Console.Write("Введите координтату y точки B: ");
-B[1] = Convert.ToInt32(Console.ReadLine());
-int res1 = (int)Math.Pow(A[0]-B[0],2);  // находим разницу, возводим значения в квадрат? (int)ставим,чтоб преобразовать значение в int
-int res2 = (int)Math.Pow(A[1]-B[1],2); // находим разницу, возводим значения в квадрат
-double dist = Math.Sqrt(res1+res2); // находим квадратный корень
-System.Console.WriteLine(Math.Round(dist, 3)); // с помощью Math.Round окургляем значение (3) - количество знаков после запятой
+// int[] A = new int[2];
+// System.Console.Write("Введите координтату x точки А: ");
+// A[0] = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write("Введите координтату y точки A: ");
+// A[1] = Convert.ToInt32(Console.ReadLine());
+// int[] B = new int[2];
+// System.Console.Write("Введите координтату x точки B: ");
+// B[0] = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write("Введите координтату y точки B: ");
+// B[1] = Convert.ToInt32(Console.ReadLine());
+// int res1 = (int)Math.Pow(A[0]-B[0],2);  // находим разницу, возводим значения в квадрат? (int)ставим,чтоб преобразовать значение в int
+// int res2 = (int)Math.Pow(A[1]-B[1],2); // находим разницу, возводим значения в квадрат
+// double dist = Math.Sqrt(res1+res2); // находим квадратный корень
+// System.Console.WriteLine(Math.Round(dist, 3)); // с помощью Math.Round окургляем значение (3) - количество знаков после запятой
+
+//Приведение типов значений
+
+double n1 = 5.8; // double - переменная вещественного типа
+int n2 = (int)n1; // приводим к целому числовому типу
+System.Console.WriteLine(n2);
